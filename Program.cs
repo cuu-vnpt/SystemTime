bec561c2234e5b9068fbead333c3cf2ef940563e
cuu-vnpt <kunnobita97@gmail.com>
2023-12-11 11:30:12 +0700

    new file

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+
+namespace SystemTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("DateTime Now is: " + localDate);
+        }
+    }
+}
